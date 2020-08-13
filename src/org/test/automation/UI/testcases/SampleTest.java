a742c8ec7114a5f59a2568ed13eb1cd4384239f0
Srinivas Nakka <snakka@splunk.com>
2020-08-13 11:45:34 +0530

    converted java proj to maven project

--- src/org/test/automation/UI/testcases/SampleTest.java
+++ src/org/test/automation/UI/testcases/SampleTest.java
@@ -5,48 +5,48 @@ import java.net.UnknownHostException;
 
 import org.test.automation.base.BrowserManager;
 import org.test.automation.exception.FrameWorkException;
+import org.test.automation.utils.DateUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import com.relevantcodes.extentreports.*;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 
 /**
  * 
  * @author Srinivas Goud Nakka
  *
  */
-public class SampleTest extends BrowserManager{
-	
+public class SampleTest extends BrowserManager {
+
 	ExtentReports extent;
 	ExtentTest logger;
-	
-	
+	String reportHtml = null;
+
 	@Test
-	public void testMethod() throws UnknownHostException, FrameWorkException
-	{
-		extent = new ExtentReports (System.getProperty("user.dir") +"/test-output/STMExtentReport.html", true);
+	public void testMethod() throws UnknownHostException, FrameWorkException {
+		reportHtml = "ExtentReports/" + this.getClass().getName() + "-" + DateUtils.DateTime() + ".html";
+		extent = new ExtentReports(reportHtml, false);
 		_Driver.get("https://service-qa.np.spanugo.com/innominds");
-		
+
 		System.out.println(_Driver.getTitle());
 		takeFullSnapShot();
-		
-		 extent
-         .addSystemInfo("Host Name", "SoftwareTestingMaterial")
-         .addSystemInfo("Environment", "Automation Testing")
-         .addSystemInfo("User Name", "Rajkumar SM");
-         //loading the external xml file (i.e., extent-config.xml) which was placed under the base directory
-         //You could find the xml file below. Create xml file in your project and copy past the code mentioned below
-         extent.loadConfig(new File(System.getProperty("user.dir")+"\\extent-config.xml"));
-         
-         logger = extent.startTest("passTest");
-         Assert.assertTrue(true);
-         //To generate the log when the test case is passed
-         logger.log(LogStatus.PASS, "Test Case Passed is passTest");
-		
-		
-		
-		
-		
+
+		extent.addSystemInfo("Host Name", "SoftwareTestingMaterial").addSystemInfo("Environment", "Automation Testing")
+				.addSystemInfo("User Name", "Rajkumar SM");
+		// loading the external xml file (i.e., extent-config.xml) which was placed
+		// under the base directory
+		// You could find the xml file below. Create xml file in your project and copy
+		// past the code mentioned below
+		extent.loadConfig(new File(System.getProperty("user.dir") + "\\extent-config.xml"));
+
+		logger = extent.startTest("passTest");
+		Assert.assertTrue(true);
+		// To generate the log when the test case is passed
+		logger.log(LogStatus.PASS, "Test Case Passed is passTest");
+
 	}
 
 }
